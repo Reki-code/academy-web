f8d9beec3bdbc6cd22dc8671568a641ad4b6f1c2
Reki-code <weiruiqi@hotmail.com>
2021-03-28 18:47:46 +0800

    add error display

--- src/components/views/Main/Role.js
+++ src/components/views/Main/Role.js
@@ -3,15 +3,20 @@ import { Redirect } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 import { ME } from '../../../graphql/user'
 import Loading from '../../common/Loading'
+import Error from '../../common/Error'
 
 const Role = () => {
-  const { data: userInfo } = useQuery(ME)
+  const { loading, error, data: userInfo } = useQuery(ME)
+
+  if (loading) return <Loading />
+  if (error) return <Error error={error} />
+
+  console.log('update')
 
   return (
     <>
-      <Loading />
-      {userInfo?.me.type === 'TEACHER' && <Redirect to='/teacher' />}
-      {userInfo?.me.type === 'STUDENT' && <Redirect to='/student' />}
+      {userInfo.me?.type === 'TEACHER' && <Redirect to='/teacher' />}
+      {userInfo.me?.type === 'STUDENT' && <Redirect to='/student' />}
     </>
   )
 }
