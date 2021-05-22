0e22ebf528c76b5fd25e85f067252b376eae6f22
reki-code <weiruiqi@hotmail.com>
2021-05-22 22:09:13 +0800

    update loading and error componment

--- src/components/common/Error.js
+++ src/components/common/Error.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import ReportProblemIcon from '@material-ui/icons/ReportProblem'
+import Typography from '@material-ui/core/Typography'
 
 const Error = ({error}) => {
   console.error('error', error)
-  return <div>{error.toString()}</div>
+  return (
+    <Grid
+      container
+      direction='column'
+      justify='center'
+      alignItems='center'
+      spacing={1}
+      py={1}
+    >
+      <ReportProblemIcon color='error' fontSize='large' />
+      <Typography variant='body1' color='error' >{error.toString()}</Typography>
+    </Grid>
+  )
 }
 
 export default Error
--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Box from '@material-ui/core/Box'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 const Loading = () => {
-  return <div>Loading</div>
+  return (
+    <Box
+      display='flex'
+      width='100%'
+      justifyContent='center'
+      py={1}
+    >
+      <CircularProgress />
+    </Box>
+  )
 }
 
 export default Loading
