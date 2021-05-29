9a2a7187e5b8ad241721569adeb8cbefd4c7daee
reki-code <weiruiqi@hotmail.com>
2021-05-30 00:05:24 +0800

    add fav to res

--- src/components/views/Resource/Resource.js
+++ src/components/views/Resource/Resource.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 import { RESOURCE_INFO } from '../../../graphql/resource'
@@ -9,12 +9,32 @@ import Box from '@material-ui/core/Box'
 import Text from './Text/Text'
 import Video from './Video/Video'
 import Slides from './Slides/Slides'
+import StarIcon from '@material-ui/icons/Star'
+import StarOutlineIcon from '@material-ui/icons/StarOutline'
+import IconButton from '@material-ui/core/IconButton'
+import ToggleIcon from 'material-ui-toggle-icon'
+import Grid from '@material-ui/core/Grid'
+import { useSnackbar } from 'notistack'
 
 const Resource = () => {
   const { resourceId } = useParams()
+  const [fav, setFav] = useState(false)
   const resourceInfo = useQuery(RESOURCE_INFO, {
     variables: { resourceId },
   })
+  const { enqueueSnackbar } = useSnackbar()
+  const handleFavorite = () => {
+    if (fav) {
+      enqueueSnackbar('取消收藏', {
+        variant: 'success'
+      })
+    } else {
+      enqueueSnackbar('收藏成功', {
+        variant: 'success'
+      })
+    }
+    setFav(!fav)
+  }
 
   if (resourceInfo.loading) return <Loading />
   if (resourceInfo.error) return <Error error={resourceInfo.error} />
@@ -26,6 +46,20 @@ const Resource = () => {
       <Typography variant='h6' >
         {resource.title}
       </Typography>
+      <Grid
+        alignContent='center'
+      >
+        <IconButton
+          onClick={handleFavorite}
+        >
+          <ToggleIcon
+            on={fav}
+            onIcon={<StarIcon fontSize='small' />}
+            offIcon={<StarOutlineIcon fontSize='small' />}
+          />
+        </IconButton>
+        <span>{fav ? '取消收藏' : '收藏'}</span>
+      </Grid>
       {resource.category === 'text' && <Text resource={resource} />}
       {resource.category === 'video' && <Video resource={resource} />}
       {resource.category === 'slides' && <Slides resource={resource} />}
