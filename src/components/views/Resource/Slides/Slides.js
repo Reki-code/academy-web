67e6a664dcfcdc322342f259a8d230046167be1d
reki-code <weiruiqi@hotmail.com>
2021-05-30 07:32:58 +0800

    fix slide

--- src/components/views/Resource/Slides/Slides.js
+++ src/components/views/Resource/Slides/Slides.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import  SlideGallery from 'react-image-gallery'
+
+const slide = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
 
 const Slides = ({ resource }) => {
+
   return (
     <div>
-      {resource.content}
+      <SlideGallery
+        showThumbnails={false}
+        
+        items={slide}
+      />
     </div>
   )
 }
